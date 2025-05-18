bbe85a30ab59a33d3e107ac653fe1b5a460dcefa
Robert Mark Bram <robertmarkbram@gmail.com>
2025-05-18 21:19:26 +0300

    Fix to ensure catalog and resource resolver is used.
    - Moving creation of schema and validator until after catalog and resource resolver are set. I can see they are being referenced now.

--- src/main/java/com/example/XBRL_validation_with_xsd_uses_xerces/XbrlValidationWithXsdUsesXercesApplication.java
+++ src/main/java/com/example/XBRL_validation_with_xsd_uses_xerces/XbrlValidationWithXsdUsesXercesApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.w3c.dom.ls.LSInput;
 import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
 import javax.xml.XMLConstants;
@@ -13,6 +14,7 @@ import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,21 +29,31 @@ public class XbrlValidationWithXsdUsesXercesApplication implements CommandLineRu
     @Override
     public void run(final String... args) throws Exception {
         // Compute path to the XBRL payload to be validated.
-        final Path xbrlPath = Path.of("src/main/resources/xbrl/xbrl_001_APRA-valid.xml");
+        validateFile(Path.of("src/main/resources/xbrl/xbrl_001_APRA-valid.xml"));
+        //        validateFile(Path.of("src/main/resources/xbrl/xbrl_003_APRA-has-errors.xml"));
+    }
+
+    private static void validateFile(final Path xbrlPath) throws SAXException, IOException {
+        System.out.println("\n\n========== " + xbrlPath + " ==========");
+
         final Source xmlStreamSource = new StreamSource(xbrlPath.toFile());
 
-        // Create objects to do the validation.
         final SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-        final Path xsdPath = Path.of(
-                "src/main/resources/xsd/sbr.gov.au/taxonomy/sbr_au_reports/sprstrm/sprcnt/sprcnt_0001/sprcnt.0001.conttrans.request.02.02.report.xsd");
-        final Schema schema = schemaFactory.newSchema(xsdPath.toFile());
 
-        // Create the validator.
-        javax.xml.validation.Validator validator = schema.newValidator();
+        final String base = "src/main/resources/xsd/www.xbrl.org/2003/";
+        final String entryPointXsd = "src/main/resources/xsd/sbr.gov.au/taxonomy/sbr_au_reports/sprstrm/sprcnt/sprcnt_0001/sprcnt.0001.conttrans.request.02.02.report.xsd";
+        StreamSource[] xsdSources = new StreamSource[]{
+                new StreamSource(entryPointXsd),
+                new StreamSource(base + "xbrl-linkbase-2003-12-31.xsd"),
+                new StreamSource(base + "xl-2003-12-31.xsd"),
+                new StreamSource(base + "xlink-2003-12-31.xsd"),
+                new StreamSource(base + "xbrl-instance-2003-12-31.xsd")
+        };
+        for (StreamSource s : xsdSources) {
+            s.setSystemId(Path.of(s.getSystemId()).toUri().toString());
+        }
 
-        // Set up a catalog so that use the local XSD files and not look them up from the internet.
-        // Set up a CatalogResolver so that use the local XSD files and not look them up from the internet.
-        final String[] catalogs = {"xsd/catalog.xml"};
+        final String[] catalogs = {"src/main/resources/xsd/catalog.xml"};
         final XMLCatalogResolver resolver = new XMLCatalogResolver(catalogs) {
             @Override
             public LSInput resolveResource(
@@ -59,15 +71,17 @@ public class XbrlValidationWithXsdUsesXercesApplication implements CommandLineRu
         };
         schemaFactory.setResourceResolver(resolver);
 
-        // Capture the XSD errors so that we can report them back.
+        final Schema schema = schemaFactory.newSchema(xsdSources);
+
+        javax.xml.validation.Validator validator = schema.newValidator();
+
         final XsdErrorHandler errorHandler = new XsdErrorHandler(xbrlPath);
         validator.setErrorHandler(errorHandler);
 
-        // Validate and extract errors.
         validator.validate(xmlStreamSource);
         if (errorHandler.getErrors().isEmpty()) {
             System.out.println("'" + xbrlPath
-                    + "' is valid against '" + xsdPath
+                    + "' is valid against '" + entryPointXsd
                     + "'.");
         }
         for (SAXParseException error : errorHandler.getErrors()) {
